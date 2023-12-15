88e7c84381b5bba8658e6d168ee9f97c93e7ff05
Wonhui Roh <rwhui012@gmail.com>
2023-12-15 17:04:17 +0900

    refactor: Coach 클래스 도입

--- src/main/kotlin/menu/controller/MenuRecommenderController.kt
+++ src/main/kotlin/menu/controller/MenuRecommenderController.kt
@@ -1,10 +1,19 @@
 package menu.controller
 
+import menu.model.Coach
 import menu.view.InputView
 
 class MenuRecommenderController {
   fun startMenuRecommender() {
     val coachNames = InputView.readCoachNames()
-    println(coachNames)
+    val coachs: List<Coach> = getCoachs(coachNames)
+  }
+
+  fun getCoachs(coachNames: List<String>): List<Coach> {
+    val coachs = mutableListOf<Coach>()
+    for (coachName in coachNames) {
+      coachs.add(Coach(coachName))
+    }
+    return coachs
   }
 }
